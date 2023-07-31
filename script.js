43f2e46222cf0dcd75e78224f38991599edec7a5
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-31 14:03:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function getSize(){
+	var w = document.documentElement.clientWidth;
+	var h = document.documentElement.clientHeight;
+
+	document.getElementById('wh').innerHTML = "<h1>Width: " + Width + " <br> Height: " + Height + "</h1>";
+}
